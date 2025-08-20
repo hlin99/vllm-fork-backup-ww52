751b2ba241b17274e51a7c909a54d3ea50ab0cc1
Tony Lin <tony.lin@intel.com>
2025-08-20 06:38:17 +0000

    Revert "[DeepSeek R1] Prefill DP under PD Dis mode (#1627)"
    This reverts commit 77e3cad4913ed603136979c1ae220af8396d041c.

--- vllm/worker/hpu_model_runner.py
+++ vllm/worker/hpu_model_runner.py
@@ -967,7 +967,8 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
         real_batch_size = len(seq_group_metadata_list)
         batch_size_padded = self.bucketing_ctx.get_padded_batch_size(
             real_batch_size, is_prompt)
-        if self.dp_awared_padding:
+        if self.dp_awared_padding and (self.vllm_config.kv_transfer_config
+                                       is None or not is_prompt):
             if self.is_driver_worker:
                 batch_size_padded = align_dp_groups(
                     batch_size_padded, torch.distributed.ReduceOp.MAX)
@@ -1192,7 +1193,10 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
             self.bucketing_ctx.get_padded_prompt_seq_len(max_query_len),
             self.block_size)
 
-        if self.dp_awared_padding:
+        # Align the prompt length across DP groups when PD aggregation is
+        # not enabled.
+        if self.dp_awared_padding and \
+            self.vllm_config.kv_transfer_config is None:
             if self.is_driver_worker:
                 max_prompt_len = align_dp_groups(
                     max_prompt_len, torch.distributed.ReduceOp.MAX)
@@ -1925,11 +1929,7 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
 
     def _dummy_run(self, max_num_batched_tokens: int) -> None:
         assert max_num_batched_tokens == 1
-        is_prompt = False
-        if self.vllm_config.kv_transfer_config is not None and \
-            self.vllm_config.kv_transfer_config.is_kv_producer:
-            is_prompt = True
-        self.warmup_scenario(max_num_batched_tokens, 1, is_prompt, None, False,
+        self.warmup_scenario(max_num_batched_tokens, 1, False, None, False,
                              True, False, 0, 1, True, True)
         return
 
