346be88a9a26f48ec3dc09eb54b3be8b844bef21
Tony Lin <tony.lin@intel.com>
2025-08-22 12:42:55 +0000

    decode warmup: 9938s prefile warmup: 4200s

--- pd_xpyd/dp_d_env.sh
+++ pd_xpyd/dp_d_env.sh
@@ -6,19 +6,19 @@ source "$BASH_DIR"/pd_env.sh
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 
 export VLLM_GPU_MEMORY_UTILIZATION=0.9
-export VLLM_GRAPH_RESERVED_MEM=0.2
+export VLLM_GRAPH_RESERVED_MEM=0.3
 export VLLM_GRAPH_PROMPT_RATIO=0
 
 # enable delayed samping on decode
 export VLLM_DELAYED_SAMPLING="true"
 
 # params
-model_len=8192
-max_num_batched_tokens=8192
-max_num_seqs=32
+model_len=32768
+max_num_batched_tokens=32768
+max_num_seqs=64
 input_min=128
-input_max=8192
-output_max=8192
+input_max=32768
+output_max=32768
 
 unset VLLM_PROMPT_BS_BUCKET_MIN VLLM_PROMPT_BS_BUCKET_STEP VLLM_PROMPT_BS_BUCKET_MAX
 unset VLLM_PROMPT_SEQ_BUCKET_MIN VLLM_PROMPT_SEQ_BUCKET_STEP VLLM_PROMPT_SEQ_BUCKET_MAX
@@ -35,8 +35,8 @@ export VLLM_PROMPT_SEQ_BUCKET_STEP=128
 export VLLM_PROMPT_SEQ_BUCKET_MAX=1
 
 #export VLLM_DECODE_BLOCK_BUCKET_MIN=2048
-export VLLM_DECODE_BS_BUCKET_STEP=1
-export VLLM_DECODE_BLOCK_BUCKET_STEP=2
+export VLLM_DECODE_BS_BUCKET_STEP=4
+export VLLM_DECODE_BLOCK_BUCKET_STEP=64
 
 echo " environments are reseted "
 
@@ -46,8 +46,9 @@ env | grep VLLM_DECODE_BS
 env | grep VLLM_DECODE_BLOCK
 
 export VLLM_SKIP_WARMUP=True
-#unset VLLM_SKIP_WARMUP
+unset VLLM_SKIP_WARMUP
 export PT_HPU_RECIPE_CACHE_CONFIG=/workspace/ww33_inc_fp8_d,false,16384
+unset PT_HPU_RECIPE_CACHE_CONFIG
 
 export VLLM_DP_SIZE=2
 export VLLM_USE_V1=0
