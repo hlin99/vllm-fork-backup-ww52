44dc14d88fd916a4968ae8adb0446ec67731eb33
Tony Lin <tony.lin@intel.com>
2025-08-26 08:18:09 +0000

    alpha

--- pd_xpyd/curl.endpoint.py
+++ pd_xpyd/curl.endpoint.py
@@ -1,7 +1,7 @@
 import requests
 
 # vLLM API 基础 URL
-BASE_URL = "http://10.239.129.9:8100"
+BASE_URL = "http://10.239.129.9:8868"
 MODEL_PATH = "/mnt/disk2/hf_models/DeepSeek-R1-G2/"
 # Endpoint 列表
 endpoints = [
