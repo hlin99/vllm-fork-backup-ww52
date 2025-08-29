40451497e83620f690be1b88d1dc5538ea7744e5
Tony Lin <tony.lin@intel.com>
2025-08-29 03:40:43 +0000

    Revert "<remove> unnecessary files"
    This reverts commit 4cc4c77e653f53266436e24e5dd78905a049b22c.

---
+++ pd_xpyd/dp1_xp4d_start_decode.sh
@@ -0,0 +1,12 @@
+BASH_DIR=$(dirname "${BASH_SOURCE[0]}")
+
+if [ -z "$1" ]; then
+    echo "please input the tp size"
+    echo "run with default mode n=1"
+    TP_SIZE=1
+else
+    TP_SIZE=$1
+fi
+
+source "$BASH_DIR"/dp_start_decode.sh g14 32 $TP_SIZE 1 "10.239.129.81"
+
