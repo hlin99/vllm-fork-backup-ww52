d5895ad9b9a9f313c18506c7dae9573858997950
Tony Lin <tony.lin@intel.com>
2025-09-17 06:34:51 +0000

    set demo1 as default master

--- pd_xpyd/1p_start_prefill.sh
+++ pd_xpyd/1p_start_prefill.sh
@@ -2,12 +2,12 @@
 BASH_DIR=$(dirname "${BASH_SOURCE[0]}")
 
 # for backward compatible. following nodes are started as mooncake master node
-if [ "$2" == "master" ] || [ -z "$1" ] || [ "$1" == "g10" ] || [ "$1" == "pcie4" ]; then
+if [ "$2" == "master" ] || [ -z "$1" ] || [ "$1" == "g10" ] || [ "$1" == "demo1" ]; then
     source "$BASH_DIR"/start_etcd_mooncake_master.sh
     echo "source "$BASH_DIR"/start_etcd_mooncake_master.sh"
 fi
 
-export MOONCAKE_CONFIG_PATH="$BASH_DIR"/mooncake_${1:-g10}.json
+export MOONCAKE_CONFIG_PATH="$BASH_DIR"/mooncake_${1:-demo1}.json
 
 echo "Using Mooncake config: $MOONCAKE_CONFIG_PATH"
 
