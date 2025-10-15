4c84efc813c65fe1414276ffba592ae27a11ff24
Tony Lin <tony.lin@intel.com>
2025-10-15 04:32:53 +0000

    change for prefill dp

---
+++ pd_xpyd/prefill_dp0_xp2d_start_decode.sh
@@ -0,0 +1,14 @@
+BASH_DIR=$(dirname "${BASH_SOURCE[0]}")
+
+source "$BASH_DIR"/start_etcd_mooncake_master.sh
+sleep 10s
+
+if [ -z "$1" ]; then
+    echo "please input the tp size"
+    echo "run with default mode n=1"
+    TP_SIZE=1
+else
+    TP_SIZE=$1
+fi
+
+source "$BASH_DIR"/dp_start_prefill.sh g10 16 $TP_SIZE 0 "10.239.129.9"
---
+++ pd_xpyd/prefill_dp1_xp2d_start_decode.sh
@@ -0,0 +1,12 @@
+BASH_DIR=$(dirname "${BASH_SOURCE[0]}")
+
+if [ -z "$1" ]; then
+    echo "please input the tp size"
+    echo "run with default mode n=1"
+    TP_SIZE=1
+else
+    TP_SIZE=$1
+fi
+
+source "$BASH_DIR"/dp_start_prefill.sh g13 16 $TP_SIZE 1 "10.239.129.9"
+
