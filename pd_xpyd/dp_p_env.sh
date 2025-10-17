18d79ccdacc98b36ff601fe19975491c662e6f64
Tony Lin <tony.lin@intel.com>
2025-10-17 04:20:08 +0000

    <tag> warm up shape

--- pd_xpyd/dp_p_env.sh
+++ pd_xpyd/dp_p_env.sh
@@ -5,7 +5,7 @@ source "$BASH_DIR"/pd_bucket.sh
 source "$BASH_DIR"/pd_env.sh
 
 
-export VLLM_GPU_MEMORY_UTILIZATION=0.7
+export VLLM_GPU_MEMORY_UTILIZATION=0.6
 export VLLM_GRAPH_RESERVED_MEM=0.1
 export VLLM_GRAPH_PROMPT_RATIO=1
 # params
@@ -13,7 +13,7 @@ model_len=16384
 max_num_batched_tokens=16384
 max_num_seqs=8
 input_min=128
-input_max=16384
+input_max=8192
 output_max=16384
 
 # ***************************************  bucketing ******************************************* #
@@ -23,6 +23,9 @@ unset VLLM_DECODE_BS_BUCKET_MIN VLLM_DECODE_BS_BUCKET_STEP VLLM_DECODE_BS_BUCKET
 unset VLLM_DECODE_BLOCK_BUCKET_MIN VLLM_DECODE_BLOCK_BUCKET_STEP VLLM_DECODE_BLOCK_BUCKET_MAX
 
 set_bucketing
+
+#export VLLM_PROMPT_BS_BUCKET_STEP=8
+export VLLM_PROMPT_SEQ_BUCKET_STEP=1024
 
 export VLLM_DECODE_BS_BUCKET_MIN=1
 export VLLM_DECODE_BS_BUCKET_STEP=1
@@ -47,7 +50,8 @@ export VLLM_EP_SIZE=8
 
 # warmup settings
 export VLLM_SKIP_WARMUP=True
-#export PT_HPU_RECIPE_CACHE_CONFIG=/workspace/pd_p_cache,false,131072
+unset VLLM_SKIP_WARMUP
+export PT_HPU_RECIPE_CACHE_CONFIG=/workspace/pd_p_cache,false,131072
 
 # MoE settings
 export VLLM_SUPPORT_MOE_CHUNK="false"  # Can be true after following para are tuned.
