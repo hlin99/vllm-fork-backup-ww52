946a0fad5054ad1caa62fa6967f55061b5fd70e3
Tony Lin <tony.lin@intel.com>
2025-11-19 06:42:50 +0000

    8k chunk

--- pd_xpyd/dp_p_env.sh
+++ pd_xpyd/dp_p_env.sh
@@ -5,16 +5,16 @@ source "$BASH_DIR"/pd_bucket.sh
 source "$BASH_DIR"/pd_env.sh
 
 
-export VLLM_GPU_MEMORY_UTILIZATION=0.343629
-export VLLM_GRAPH_RESERVED_MEM=0.02
+export VLLM_GPU_MEMORY_UTILIZATION=0.3475
+export VLLM_GRAPH_RESERVED_MEM=0.0311
 export VLLM_GRAPH_PROMPT_RATIO=1
 
 # params
 model_len=131072
 max_num_batched_tokens=8192
-max_num_seqs=64
+max_num_seqs=16
 input_min=128
-input_max=128
+input_max=8192
 output_max=8192
 
 export VLLM_CONTIGUOUS_PA=false
